3654663a1159be23ecee550647f0bf1acf64e467
Nuno Martins <nmartins@redhat.com>
2026-02-23 13:54:34 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -3,12 +3,31 @@
 systemctl stop systemd-tmpfiles-setup.service
 systemctl disable systemd-tmpfiles-setup.service
 
+retry() {
+    for i in {1..3}; do
+        echo "Attempt $i: $2"
+        if $1; then
+            return 0
+        fi
+        [ $i -lt 3 ] && sleep 5
+    done
+    echo "Failed after 3 attempts: $2"
+    exit 1
+}
+
+retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
+retry "update-ca-trust"
+retry "rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm"
+retry "subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}"
+retry "dnf install python3 python3-pip"
+
+
 # # Install collection(s)
 # ansible-galaxy collection install ansible.eda
 # ansible-galaxy collection install community.general
 # ansible-galaxy collection install ansible.windows
 # ansible-galaxy collection install microsoft.ad
-dnf install python3 python3-pip -y
+
 pip3 install ansible-sign
 # # ## setup rhel user
 touch /etc/sudoers.d/rhel_sudoers
